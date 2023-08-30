63a7ca023bde904c49d26872a46983df4c0c4408
Md Yusuf <mdyusuf0605@gmail.com>
2023-08-30 21:45:20 +0600

    show details modal added

--- js/phone.js
+++ js/phone.js
@@ -1,24 +1,21 @@
-
-const loadPhone = async (searchText, isShowAll) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+const loadPhone = async (searchText = 13, isShowAll) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
     displayPhones(phones, isShowAll);
-}
+};
 
 const displayPhones = (phones, isShowAll) => {
-
-    const phoneContainer = document.getElementById('phone-container');
+    const phoneContainer = document.getElementById("phone-container");
     // clear container before adding new search
-    phoneContainer.textContent = '';
+    phoneContainer.textContent = "";
 
     // show all button if phones > 12
-    const showAll = document.getElementById('show-all');
+    const showAll = document.getElementById("show-all");
     if (phones.length > 12 && !isShowAll) {
-        showAll.classList.remove('hidden');
-    }
-    else {
-        showAll.classList.add('hidden');
+        showAll.classList.remove("hidden");
+    } else {
+        showAll.classList.add("hidden");
     }
 
     // show only 12 element of data
@@ -26,42 +23,66 @@ const displayPhones = (phones, isShowAll) => {
         phones = phones.slice(0, 12);
     }
 
-    phones.forEach(phone => {
-        const phoneCard = document.createElement('div');
+    phones.forEach((phone) => {
+        const phoneCard = document.createElement("div");
         phoneCard.classList = `card w-96 p-4 bg-red-100 shadow-xl`;
         phoneCard.innerHTML = `
             <figure><img src="${phone.image}" alt="Shoes" /></figure>
             <div class="card-body">
                 <h2 class="card-title">${phone.phone_name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+                <div class="card-actions justify-center">
+                <button onclick="showModal('${phone.slug}')" class="btn btn-accent">Show Details</button>
                 </div>
             </div>
-        `
-        phoneContainer.appendChild(phoneCard)
+        `;
+        phoneContainer.appendChild(phoneCard);
     });
     loadingSpinner(false);
-}
+};
 
 function phoneSearch(isShowAll) {
     loadingSpinner(true);
-    const searchField = document.getElementById('search-field');
+    const searchField = document.getElementById("search-field");
     const searchText = searchField.value;
     loadPhone(searchText, isShowAll);
-
 }
 // loading spinner
 const loadingSpinner = (isLoading) => {
-    const spinnerLoading = document.getElementById('loading-spinner');
+    const spinnerLoading = document.getElementById("loading-spinner");
     if (isLoading) {
-        spinnerLoading.classList.remove('hidden');
+        spinnerLoading.classList.remove("hidden");
+    } else {
+        spinnerLoading.classList.add("hidden");
     }
-    else {
-        spinnerLoading.classList.add('hidden');
-    }
-}
+};
 
 const handleShowAll = () => {
     phoneSearch(true);
-}
+};
+
+const showModal = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    const phone = data.data;
+    showPhoneDetails(phone);
+};
+
+// show phone details in modal
+
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const showPhoneName = document.getElementById("show-phone-name");
+    showPhoneName.innerText = phone.name;
+    const showDetailsContainer = document.getElementById("show-details-container");
+    showDetailsContainer.innerHTML = `
+        <img src="${phone.image}" alt="" >
+        <p>Phone Storage:<span>${phone?.mainFeatures?.storage}</span></p>
+        <p>Phone memory:<span>${phone?.mainFeatures?.memory}</span></p>
+        <p>GPS:<span>${phone?.others?.GPS || 'no gps'}</span></p>
+    `;
+
+    show_modal.showModal();
+};
+
+loadPhone();
